0f32427fd51041ceae5a600d68355d6fb296be65
Chisaiibeeboo <kate.la@gmail.com>
2014-08-07 10:21:52 -0700

    created box

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,17 +1,4 @@
-$(document).ready( function genDivs(v){ 
-      var e = document.body; // whatever you want to append the rows to: 
-      for(var i = 0; i < v; i++){ 
-        var row = document.createElement("div"); 
-        row.className = "row"; 
-        for(var x = 1; x <= v; x++){ 
-            var cell = document.createElement("div"); 
-            cell.className = "gridsquare"; 
-            cell.innerText = (i * v) + x;
-            row.appendChild(cell); 
-        } 
-        e.appendChild(row); 
-      } 
-      document.getElementById("code").innerText = e.innerHTML;
-
-    };
+$(document).ready(function() {
+  $('div').css({margin: 'auto'});
+});
 
